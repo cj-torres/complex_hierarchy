5bcd340849afb75575fde2ddfb8262ee09585921
cj-torres <torrescj92@gmail.com>
2022-10-03 12:01:01 -0700

    aggressive memory management

--- main.py
+++ main.py
@@ -55,11 +55,12 @@ def run_transf_seq(num, filename, generator_function, **kwargs):
                 language_set = generator_function(**kwargs)
                 print("Model %d" % (i + 1))
                 model = TransformerSequencer(4, 4, 2, 1, 4, 3)
-                for model_out, best_loss, percent_correct, epoch in seq_train(model, language_set, 256, 20, 10000):
+                for model_out, best_loss, percent_correct, epoch in seq_train(model, language_set, 128, 20, 10000):
                     weights = model_to_list(model_out)
                     writer_weights.writerow(weights)
                     encoding_cost = sum([1 / 2 * w ** 2 for w in weights])
                     writer_details.writerow([i+1, best_loss.item(), percent_correct.item(), epoch, encoding_cost])
+                    del model, model_out
 
 
 if __name__ == '__main__':
@@ -68,14 +69,14 @@ if __name__ == '__main__':
     N = 100
     new_dir = "Output-{}".format(str(date.today()))
     os.mkdir(new_dir)
-    run_seq(N, "{}/dyck1_small_lstm".format(new_dir), lb.make_dyck1_sets_uniform,
-            **{"N": 1000, "p": .01, "reject_threshold": 200, "split_p": .795})
-    run_seq(N, "{}/a2nb2m_small_lstm".format(new_dir), lb.make_a2nb2m_sets,
-            **{"p":.01, "N": 1000, "reject_threshold": 200, "split_p": .795})
-    run_seq(N, "{}/abn_small_lstm".format(new_dir), lb.make_abn_sets,
-            **{"p":.01, "N": 1000, "reject_threshold": 200, "split_p": .795})
-    run_seq(N, "{}/anbn_small_lstm".format(new_dir), lb.make_anbn_sets,
-            **{"p":.01, "N": 1000, "reject_threshold": 200, "split_p": .795})
+    #run_seq(N, "{}/dyck1_small_lstm".format(new_dir), lb.make_dyck1_sets_uniform,
+    #        **{"N": 1000, "p": .01, "reject_threshold": 200, "split_p": .795})
+    #run_seq(N, "{}/a2nb2m_small_lstm".format(new_dir), lb.make_a2nb2m_sets,
+    #        **{"p":.01, "N": 1000, "reject_threshold": 200, "split_p": .795})
+    #run_seq(N, "{}/abn_small_lstm".format(new_dir), lb.make_abn_sets,
+    #        **{"p":.01, "N": 1000, "reject_threshold": 200, "split_p": .795})
+    #run_seq(N, "{}/anbn_small_lstm".format(new_dir), lb.make_anbn_sets,
+    #        **{"p":.01, "N": 1000, "reject_threshold": 200, "split_p": .795})
     run_transf_seq(N, "{}/dyck1_small_trans".format(new_dir), lb.make_dyck1_sets_uniform,
             **{"N": 1000, "p": .01, "reject_threshold": 200, "split_p": .795})
     run_transf_seq(N, "{}/a2nb2m_small_trans".format(new_dir), lb.make_a2nb2m_sets,
