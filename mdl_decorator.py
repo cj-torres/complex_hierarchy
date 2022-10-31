ef6e064949be9731df76595e48252a4fde11c499
cj-torres <torrescj92@gmail.com>
2022-10-31 14:10:34 -0700

    Update mdl_decorator.py

--- mdl_decorator.py
+++ mdl_decorator.py
@@ -6,6 +6,7 @@ class L0_Regularizer(torch.nn.Module):
     def __init__(self, original_module: torch.nn.Module, lam: float,
                       weight_decay: float = 0, is_neural: bool = False,
                     temperature: float = 2/3, droprate_init=0.5,
+                 limit_a = -.1, limit_b = 1.1, epsilon = 1e-6
                  ):
         # is_neural support only for models consisting of 1-d and 2-d tensors
         # other support could be chaotic
@@ -15,19 +16,25 @@ class L0_Regularizer(torch.nn.Module):
         self.pre_parameters = torch.nn.ParameterDict(
             {name + "_p": param for name, param in self.module.named_parameters()}
         )
+
+        self.param_names = [name for name, param in self.module.named_parameters()]
         self.is_neural = is_neural
         self.mask_parameters = torch.nn.ParameterDict()
-        self.original_forward = self.forward
+        self.module.original_forward = self.module.forward
         self.lam = lam
         self.weight_decay = weight_decay
         self.temperature = temperature
         self.droprate_init = droprate_init
+        self.limit_a = limit_a
+        self.limit_b = limit_b
+        self.epsilon = epsilon
 
         if is_neural:
             for name, param in self.pre_parameters.items():
                 if len(param.size()) == 2:
                     mask = torch.nn.Parameter(torch.Tensor(param.size()[0]))
                     self.mask_parameters.update({name + "_m": mask})
+
         else:
             for name, param in self.pre_parameters.items():
                 mask = torch.nn.Parameter(torch.Tensor(param.size()))
@@ -56,29 +63,42 @@ class L0_Regularizer(torch.nn.Module):
         for name, weight in self.module.mask_parameters():
             weight.data.clamp_(min=math.log(1e-2), max=math.log(1e2))
 
-    def cdf_qz(self, x):
+    def cdf_qz(self, x, param):
         """Implements the CDF of the 'stretched' concrete distribution"""
-        xn = (x - limit_a) / (limit_b - limit_a)
+        '''references parameters'''
+        xn = (x - self.limit_a) / (self.limit_b - self.limit_a)
         logits = math.log(xn) - math.log(1 - xn)
-        return torch.nn.functional.sigmoid(logits * self.temperature - self.qz_loga).clamp(min=epsilon, max=1 - epsilon)
+        return torch.nn.functional.sigmoid(
+            logits * self.temperature - self.mask_parameters[param+"_m"]).clamp(min=self.epsilon, max=1 - self.epsilon)
 
-    def quantile_concrete(self, x):
+    def quantile_concrete(self, x, param):
         """Implements the quantile, aka inverse CDF, of the 'stretched' concrete distribution"""
-        y = torch.nn.functional.sigmoid((torch.log(x) - torch.log(1 - x) + self.qz_loga) / self.temperature)
-        return y * (limit_b - limit_a) + limit_a
+        '''references parameters'''
+        y = torch.nn.functional.sigmoid(
+            (torch.log(x) - torch.log(1 - x) + self.mask_parameters[param+"_m"]) / self.temperature)
+        return y * (self.limit_b - self.limit_a) + self.limit_a
 
     def _reg_w(self, param):
         """Expected L0 norm under the stochastic gates, takes into account and re-weights also a potential L2 penalty"""
-        if self.is_neural:
-            logpw_col = torch.sum(- (.5 * self.weight_decay * self.param.pow(2)) - self.lam, 1)
-            logpw = torch.sum((1 - self.cdf_qz(0)) * logpw_col)
-            logpb = 0 if not self.use_bias else - torch.sum(.5 * self.prior_prec * self.bias.pow(2))
-        else:
+        '''is_neural is old method, calculates wrt columns first multiplied by expected values of gates
+           second method calculates wrt all parameters
+        '''
 
-        return logpw + logpb
+        # why is this negative? will investigate behavior at testing
+        if self.is_neural:
+            logpw_col = torch.sum(- (.5 * self.weight_decay * self.pre_parameters[param+"_p"].pow(2)) - self.lam, 1)
+            logpw = torch.sum((1 - self.cdf_qz(0, param)) * logpw_col)
+        else:
+            logpw_l2 = - (.5 * self.weight_decay * param.pow(2)) - self.lam
+            logpw = torch.sum((1 - self.cdf_qz(0, param)) * logpw_l2)
+
+        return logpw
 
     def regularization(self):
-        return self._reg_w()
+        r_total = torch.Tensor([])
+        for param in self.param_names:
+            r_total = torch.cat([r_total, self._reg_w(param)])
+        return r_total.sum()
 
     def count_expected_flops_and_l0(self):
         """Measures the expected floating point operations (FLOPs) and the expected L0 norm"""
@@ -95,8 +115,8 @@ class L0_Regularizer(torch.nn.Module):
 
     def get_eps(self, size):
         """Uniform random numbers for the concrete distribution"""
-        eps = self.floatTensor(size).uniform_(epsilon, 1-epsilon)
-        eps = Variable(eps)
+        # Variable deprecated and removed
+        eps = self.floatTensor(size).uniform_(self.epsilon, 1-self.epsilon)
         return eps
 
     def sample_z(self, batch_size, sample=True):
@@ -104,10 +124,10 @@ class L0_Regularizer(torch.nn.Module):
         if sample:
             eps = self.get_eps(self.floatTensor(batch_size, self.in_features))
             z = self.quantile_concrete(eps)
-            return F.hardtanh(z, min_val=0, max_val=1)
+            return torch.nn.functional.hardtanh(z, min_val=0, max_val=1)
         else:  # mode
-            pi = F.sigmoid(self.qz_loga).view(1, self.in_features).expand(batch_size, self.in_features)
-            return F.hardtanh(pi * (limit_b - limit_a) + limit_a, min_val=0, max_val=1)
+            pi = torch.nn.functional.sigmoid(self.qz_loga).view(1, self.in_features).expand(batch_size, self.in_features)
+            return torch.nn.functional.hardtanh(pi * (self.limit_b - self.limit_a) + self.limit_a, min_val=0, max_val=1)
 
     def sample_weights(self):
         z = self.quantile_concrete(self.get_eps(self.floatTensor(self.in_features)))
